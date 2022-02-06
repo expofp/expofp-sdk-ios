f43d9a37a3f84b756158d8fc934426d6ef07aee0
Vladimir <vladimir@expofp.com>
2022-02-06 22:31:10 +0300

    1.0.0

--- FplanKit/FplanView.swift
+++ FplanKit/FplanView.swift
@@ -77,8 +77,8 @@ public struct FplanView: UIViewRepresentable {
     private let routeBuildHandler: ((_ route: Route) -> Void)?
     
     public init(_ url: String, fplanReadyHandler: (() -> Void)? = nil,
-         boothSelectionHandler: ((_ boothName: String) -> Void)? = nil,
-         routeBuildHandler: ((_ route: Route) -> Void)? = nil){
+                boothSelectionHandler: ((_ boothName: String) -> Void)? = nil,
+                routeBuildHandler: ((_ route: Route) -> Void)? = nil){
         
         self.url = url
         self.fplanReadyHandler = fplanReadyHandler
@@ -107,21 +107,55 @@ public struct FplanView: UIViewRepresentable {
         webView.evaluateJavaScript("window.selectRoute('\(from)', '\(to)', \(exceptUnaccessible))")
     }
     
+    public func setCurrentPosition(_ x: Int, _ y: Int, _ focus: Bool = false){
+        webView.evaluateJavaScript("window.setCurrentPosition('\(x)', '\(y)', \(focus))")
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
     public func updateUIView(_ webView: WKWebView, context: Context) {
-        if let requestUrl = URL(string: url) {
-            webView.load(URLRequest(url: requestUrl))
-            
-            if let handle = fplanReadyHandler{
-                webView.configuration.userContentController.add(FpHandler(handle), name: "onFpConfiguredHandler")
+        let netReachability = NetworkReachability()
+        let isConnected = netReachability.checkConnection()
+        
+        if let htmlPath = Bundle.main.path(forResource: "index", ofType: "html") {
+            do {
+                let eventId = url.starts(with: "https://")
+                ? url[url.index(url.startIndex, offsetBy: 8)...url.index(url.firstIndex(of: ".")!, offsetBy: -1)]
+                : url[...url.index(url.firstIndex(of: ".")!, offsetBy: -1)]
+                
+                let html = try String(contentsOfFile: htmlPath, encoding: .utf8)
+                    .replacingOccurrences(of: "$url#", with: url)
+                    .replacingOccurrences(of: "$eventId#", with: eventId)
+                
+                let filename = getDocumentsDirectory().appendingPathComponent("index.html")
+                try html.write(to: filename, atomically: true, encoding: String.Encoding.utf8)
+                
+                if(isConnected){
+                    let requestUrl = URLRequest(url: filename, cachePolicy: .reloadRevalidatingCacheData)
+                    webView.load(requestUrl)
+                }
+                else{
+                    let requestUrl = URLRequest(url: filename, cachePolicy: .returnCacheDataElseLoad)
+                    webView.load(requestUrl)
+                }
+                
+            } catch {
             }
-                        
-            if let handle = boothSelectionHandler{
-                webView.configuration.userContentController.add(BoothHandler(handle), name: "onBoothClickHandler")
-            }
-            
-            if let handle = routeBuildHandler{
-                webView.configuration.userContentController.add(RouteHandler(handle), name: "onDirectionHandler")
-            }
+        }
+        
+        if let handle = fplanReadyHandler{
+            webView.configuration.userContentController.add(FpHandler(handle), name: "onFpConfiguredHandler")
+        }
+        
+        if let handle = boothSelectionHandler{
+            webView.configuration.userContentController.add(BoothHandler(handle), name: "onBoothClickHandler")
+        }
+        
+        if let handle = routeBuildHandler{
+            webView.configuration.userContentController.add(RouteHandler(handle), name: "onDirectionHandler")
         }
     }
 }
@@ -130,7 +164,8 @@ public struct FplanView_Previews: PreviewProvider {
     public init(){
         
     }
+    @available(iOS 13.0.0, *)
     public static var previews: some View {
-        FplanView("https://developer.expofp.com/examples/autumnfair.html")
+        FplanView("https://wayfinding.expofp.com")
     }
 }
